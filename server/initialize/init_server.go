64598de3768324a6ab40d0defcbb4509f2cf3304
Nathan M.S <natms24@gmail.com>
2024-11-14 19:37:40 +0300

    chore: refactor initalization code

---
+++ server/initialize/init_server.go
@@ -0,0 +1,74 @@
+package initialize
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+var clients = make(map[net.Conn]bool)
+var mu sync.Mutex
+
+func removeClient(conn net.Conn) {
+	mu.Lock()
+	delete(clients, conn)
+	mu.Unlock()
+	conn.Close()
+}
+
+func handleClient(conn net.Conn) {
+	deadlineSeconds := 3
+	err := conn.SetDeadline(time.Now().Add(time.Second * time.Duration(deadlineSeconds)))
+	if err != nil {
+		log.Println("failed to set connection deadline:", err)
+		removeClient(conn)
+		return
+	}
+
+	reader := bufio.NewReader(conn)
+	for {
+		connErr := conn.SetDeadline(time.Now().Add(time.Second * time.Duration(deadlineSeconds)))
+		if connErr != nil {
+			log.Println("failed to set connection deadline:", connErr)
+			removeClient(conn)
+			return
+		}
+
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("client disconnected:", err)
+			conn.Close()
+			return
+		}
+
+		conn.Write([]byte(message))
+		fmt.Print("received:", message)
+	}
+}
+
+func InitServer(PORT int) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", PORT))
+	if err != nil {
+		log.Fatalf("failed to listen on port %v: %v\n", PORT, err.Error())
+	}
+
+	defer listener.Close()
+	log.Println("Server listening on port:", PORT)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("error accepting connection:", err)
+			continue
+		}
+
+		mu.Lock()
+		clients[conn] = true
+		mu.Unlock()
+
+		go handleClient(conn)
+	}
+}
--- server/service/service.go
+++ server/service/service.go
@@ -1,1 +1,21 @@
 package service
+
+import "distributed-key-value-store/server/domain"
+
+type Service struct{}
+
+func (service *Service) Get(key string) domain.DomainError {
+	panic("not implemented") // TODO: Implement
+}
+
+func (service *Service) Put(key string, value string) domain.DomainError {
+	panic("not implemented") // TODO: Implement
+}
+
+func (service *Service) Delete(key string) domain.DomainError {
+	panic("not implemented") // TODO: Implement
+}
+
+func (service *Service) List() domain.DomainError {
+	panic("not implemented") // TODO: Implement
+}

11b96059c4ed1d036fc7e40bddbfc23398f7e4b8
Nathan M.S <natms24@gmail.com>
2024-11-14 15:13:48 +0300

    feat: add server skeleton

--- server/main.go
+++ server/main.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+var clients = make(map[net.Conn]bool)
+var mu sync.Mutex
+
+func main() {
+	PORT := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", PORT))
+	if err != nil {
+		log.Fatalf("failed to listen on port %v: %v\n", PORT, err.Error())
+	}
+
+	defer listener.Close()
+	log.Println("Server listening on port:", PORT)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("error accepting connection:", err)
+			continue
+		}
+
+		mu.Lock()
+		clients[conn] = true
+		mu.Unlock()
+
+		go handleClient(conn)
+	}
+}
+
+func removeClient(conn net.Conn) {
+	mu.Lock()
+	delete(clients, conn)
+	mu.Unlock()
+	conn.Close()
+}
+
+func handleClient(conn net.Conn) {
+	deadlineSeconds := 100
+	err := conn.SetDeadline(time.Now().Add(time.Second * time.Duration(deadlineSeconds)))
+	if err != nil {
+		log.Println("failed to set connection deadline:", err)
+		removeClient(conn)
+		return
+	}
+
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("client disconnected:", err)
+		}
+
+		fmt.Println("Received: ", message)
+	}
+}
